6d29135d9ded2c7cc7f1eaa29b247b760b835733
yoona1110 <gjdbsdk18@icloud.com>
2024-04-21 01:34:21 +0900

    Feat: molecules/WriteInput 스토리 추가

---
+++ src/component/molecules/WriteInput/styles.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const WriteInputLayout = styled.text`
+  width: 332px;
+  height: 140px;
+`;
+
+export const WriteHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 215px;
+  height: 18px;
+`;
+
+export const Line = styled.div`
+  background-color: #BDBDBD;
+  width: 345px;
+  height: 0.75px;
+  margin: 10px 0;
+`;
+
+export const WriteTitleText = styled.text`
+  font-size: 20px;
+  font-weight: bold;
+`;
