8d7bfa883ad495f00d7d888030231056478f64df
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2024-05-04 14:18:44 +0900

    Feat: Template/SignUpSettingMajor

---
+++ src/component/Template/SignUp/SignUpSettingMajor/styles.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const SignUpSettingSchoolNameLayout = styled.div`
+  width: 375px;
+  height: 812px;
+  display: flex;
+  flex-direction: column;
+
+  & > button:first-child {
+    margin: 17px 16px 0 0;
+  }
+`;
+
+export const SignUpSettingSchoolNameText = styled.p`
+  margin: 46px 18px 0 0;
+  height: 65px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const SignUpSettingSchoolNameLabelForm = styled.form`
+  margin: 100px 18px 0 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+
+  & button {
+    margin-top: 367px;
+  }
+`;
+export const SignUpSettingSchoolNameLabel = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 68px;
+  justify-content: space-between;
+`;
+
+export const SighUpSchoolSettingHeader = styled.header`
+  display: flex;
+  align-items: center;
+
+  & span {
+    margin-left: 112px;
+  }
+`;
+
+export const SignUpSettingSchoolDoubleMajorLabel = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 68px;
+  justify-content: space-between;
+  margin-top: 28px;
+`;
