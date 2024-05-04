05ab69405f154eccbb48f4ed3de6b419fc1084ca
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2024-05-04 12:51:21 +0900

    Fix: 로그인창 디자인 반영

--- src/component/Template/LoginPage/styles.ts
+++ src/component/Template/LoginPage/styles.ts
@@ -5,27 +5,10 @@ export const LoginPageLayout = styled.div`
   height: 100vh;
   display: flex;
   flex-direction: column;
-  align-items: center;
-`;
-
-export const MainLogo = styled.img``;
-
-export const LoginPageHeader = styled.header`
-  height: 421px;
-  display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
 `;
 
-export const LoginPageLoginForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  & input:not(:first-child) {
-    margin-top: 10px;
-  }
-  & button:not(:last-child) {
-    margin-top: 53px;
-    margin-bottom: 10px;
-  }
+export const KaKaoLogin = styled.img`
+  margin-top: 191px;
 `;
