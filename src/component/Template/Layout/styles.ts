e5f002a8a4ac66aa2056936c121d1027c2af5ee7
yoona1110 <gjdbsdk18@icloud.com>
2024-06-02 12:46:34 +0900

    Design: 페이지 너비 수정

--- src/component/Template/Layout/styles.ts
+++ src/component/Template/Layout/styles.ts
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 export const DefaultLayout = styled.div`
-  width: 100%;
+  width: 375px;
   height: 100%;
 
   & > button {
     position: fixed;
-    right: 44px;
-    bottom: 123px;
+    right: 625px;
+    bottom: 125px;
   }
 `;
