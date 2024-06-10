dadf70de0b9adf892b28bffda6d168b116cbcbd0
yoona1110 <gjdbsdk18@icloud.com>
2024-06-10 22:00:55 +0900

    Fix: login 페이지 이미지 및 텍스트 추가

--- src/component/Template/Login/LoginMainPage/index.tsx
+++ src/component/Template/Login/LoginMainPage/index.tsx
@@ -1,11 +1,17 @@
 import { FC } from "react";
-import { LoginMainPageLayout, KaKaoLogin } from "./styles";
+import { LoginMainPageLayout, MainLogo, KaKaoLogin } from "./styles";
 import { LoginProps } from "@type/Tb";
 import KaKaoLoginIcon from "@asset/image/icon_KaKaoLogin.png";
+import MainLogoIcon from "@asset/image/Logo/Logo_mainLogo01.png";
+import Heading from "@atom/Heading";
 
 const LoginMainPage: FC<LoginProps> = ({ onclick }) => {
   return (
     <LoginMainPageLayout>
+      <Heading fontSize={"24px"} fontWeight={"600"} color={"rgba(0, 0, 0, 1"}>
+        팀원을 모집할 땐? <br/> 팀모아
+      </Heading>
+      <MainLogo src={MainLogoIcon}/>
       <KaKaoLogin src={KaKaoLoginIcon} alt="/" onClick={onclick} />
     </LoginMainPageLayout>
   );
--- src/component/Template/Login/LoginMainPage/styles.ts
+++ src/component/Template/Login/LoginMainPage/styles.ts
@@ -5,12 +5,25 @@ export const LoginMainPageLayout = styled.div`
   height: 100vh;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
+  overflow-x: hidden;
+
+  & h3 {
+    padding-top: 83px;
+    line-height: 40px;
+    margin-right: 160px;
+  }
+`;
+
+export const MainLogo = styled.img`
+  width: 260px;
+  height: 260px;
+  transform: rotate(-15deg);
+  margin: 77px 0 31.57px 0;
+  margin-left: 180px;
 `;
 
 export const KaKaoLogin = styled.img`
-  margin-top: 191px;
   width: 203.06px;
   height: 50px;
 `;
